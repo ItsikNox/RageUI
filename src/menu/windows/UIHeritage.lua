739acf063c208246fa4afb89cee9fd81b7131875
Dylan Malandain <lucenzo.76@gmail.com>
2019-04-21 17:12:35 +0200

    Add more LUA Docs UIHeritage

--- src/menu/windows/UIHeritage.lua
+++ src/menu/windows/UIHeritage.lua
@@ -4,9 +4,13 @@
 ---@return nil
 ---@public
 function RageUI.HeritageWindow(Mum, Dad)
+
+    ---@type table
     local CurrentMenu = RageUI.CurrentMenu;
+
     if CurrentMenu ~= nil then
         if CurrentMenu() then
+
             if Mum < 0 or Mum > 21 then
                 Mum = 0
             end
@@ -23,11 +27,14 @@ function RageUI.HeritageWindow(Mum, Dad)
             else
                 Dad = "male_" .. Dad
             end
+
+            ---@type table
             local Heritage = RageUI.Settings.Windows.Heritage;
 
             RageUI.RenderSprite(Heritage.Background.Dictionary, Heritage.Background.Texture, CurrentMenu.X, CurrentMenu.Y + CurrentMenu.SubtitleHeight + RageUI.ItemOffset, Heritage.Background.Width + (CurrentMenu.WidthOffset / 1), Heritage.Background.Height)
             RageUI.RenderSprite(Heritage.Dad.Dictionary, Dad, CurrentMenu.X + Heritage.Dad.X + (CurrentMenu.WidthOffset / 2), CurrentMenu.Y + CurrentMenu.SubtitleHeight + RageUI.ItemOffset, Heritage.Dad.Width, Heritage.Dad.Height)
             RageUI.RenderSprite(Heritage.Mum.Dictionary, Mum, CurrentMenu.X + Heritage.Mum.X + (CurrentMenu.WidthOffset / 2), CurrentMenu.Y + CurrentMenu.SubtitleHeight + RageUI.ItemOffset, Heritage.Mum.Width, Heritage.Mum.Height)
+
             RageUI.ItemOffset = RageUI.ItemOffset + Heritage.Background.Height
         end
     end
