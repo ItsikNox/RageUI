30897bb9d9f2bc3e40e5acbee2123502912669b8
Dylan Malandain <lucenzo.76@gmail.com>
2019-07-24 02:54:22 +0200

    CleanUp Project elements

--- __resource.lua
+++ __resource.lua
@@ -1,34 +1,36 @@
 resource_manifest_version '05cfa83c-a124-4cfa-a768-c24a5811d8f9'
 
 client_scripts {
-    "src/menu/RageUI.lua",
-    "src/menu/Menu.lua",
-    "src/menu/Submenu.lua",
-    "src/menu/Controller.lua",
+    "components/Rectangle.lua",
+    "components/Sprite.lua",
+    "components/Text.lua",
+    "components/Visual.lua",
 
-    "src/elements/Rectangle.lua",
-    "src/elements/Sprite.lua",
-    "src/elements/Text.lua",
-    "src/elements/Visual.lua",
+    "menu/wrapper/items-wrapper.lua",
 
-    "src/menu/wrapper/items-wrapper.lua",
+    "menu/elements/ItemsBadge.lua",
+    "menu/elements/ItemsColour.lua",
+    "menu/elements/PanelColour.lua",
+    "menu/items/UIButton.lua",
+    "menu/items/UICheckBox.lua",
+    "menu/items/UIList.lua",
+    "menu/items/UIProgress.lua",
+    "menu/items/UISlider.lua",
 
-    "src/menu/elements/Badge.lua",
-    "src/menu/elements/ColourPanel.lua",
-    "src/menu/items/UIButton.lua",
-    "src/menu/items/UICheckBox.lua",
-    "src/menu/items/UIList.lua",
-    "src/menu/items/UIProgress.lua",
-    "src/menu/items/UISlider.lua",
+    "menu/panels/UIColourPanel.lua",
+    "menu/panels/UIGridPanel.lua",
+    "menu/panels/UIGridPanelHorizontal.lua",
+    "menu/panels/UIGridPanelVertical.lua",
+    "menu/panels/UIPercentagePanel.lua",
+    "menu/windows/UIHeritage.lua",
 
-    "src/menu/panels/UIColourPanel.lua",
-    "src/menu/panels/UIGridPanel.lua",
-    "src/menu/panels/UIGridPanelHorizontal.lua",
-    "src/menu/panels/UIGridPanelVertical.lua",
-    "src/menu/panels/UIPercentagePanel.lua",
-    "src/menu/windows/UIHeritage.lua",
+    "menu/RageUI.lua",
+    "menu/Menu.lua",
+    "menu/Submenu.lua",
+    "menu/Controller.lua",
 
-    "MenuExample/MenuExample.lua",
+
+    "example.lua",
 }
 
 
