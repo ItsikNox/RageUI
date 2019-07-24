e46f8364ce023253527cd1c1369a1dfa4e73995a
Dylan Malandain <lucenzo.76@gmail.com>
2019-07-24 18:46:04 +0200

    Fix resource lua Path for all RageUI content

--- __resource.lua
+++ __resource.lua
@@ -1,6 +1,10 @@
 resource_manifest_version '05cfa83c-a124-4cfa-a768-c24a5811d8f9'
 
 client_scripts {
+    "rageui/RageUI.lua",
+    "rageui/Menu.lua",
+    "rageui/Controller.lua",
+
     "components/Rectangle.lua",
     "components/Sprite.lua",
     "components/Text.lua",
@@ -9,26 +13,22 @@ client_scripts {
     "components/Scaleform.lua",
     "components/Screen.lua",
 
-    "menu/elements/ItemsBadge.lua",
-    "menu/elements/ItemsColour.lua",
-    "menu/elements/PanelColour.lua",
-    "menu/items/UIButton.lua",
-    "menu/items/UICheckBox.lua",
-    "menu/items/UIList.lua",
-    "menu/items/UIProgress.lua",
-    "menu/items/UISlider.lua",
+    "rageui/elements/ItemsBadge.lua",
+    "rageui/elements/ItemsColour.lua",
+    "rageui/elements/PanelColour.lua",
+    "rageui/items/UIButton.lua",
+    "rageui/items/UICheckBox.lua",
+    "rageui/items/UIList.lua",
+    "rageui/items/UIProgress.lua",
+    "rageui/items/UISlider.lua",
 
-    "menu/panels/UIColourPanel.lua",
-    "menu/panels/UIGridPanel.lua",
-    "menu/panels/UIGridPanelHorizontal.lua",
-    "menu/panels/UIGridPanelVertical.lua",
-    "menu/panels/UIPercentagePanel.lua",
-    "menu/windows/UIHeritage.lua",
+    "rageui/panels/UIColourPanel.lua",
+    "rageui/panels/UIGridPanel.lua",
+    "rageui/panels/UIGridPanelHorizontal.lua",
+    "rageui/panels/UIGridPanelVertical.lua",
+    "rageui/panels/UIPercentagePanel.lua",
+    "rageui/windows/UIHeritage.lua",
 
-    "menu/RageUI.lua",
-    "menu/Menu.lua",
-    "menu/Submenu.lua",
-    "menu/Controller.lua",
 
 
     "example.lua",
