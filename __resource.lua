9fcb0b23117e0da2e5f892dbb2c47290dd1205de
Dylan Malandain <lucenzo.76@gmail.com>
2019-07-25 23:52:54 +0200

    Fix merge conflicts

--- __resource.lua
+++ __resource.lua
@@ -1,7 +1,7 @@
 resource_manifest_version '05cfa83c-a124-4cfa-a768-c24a5811d8f9'
 
 client_scripts {
-    "rageui/RageUI.lua",
+    "rageui/Base.lua",
     "rageui/Menu.lua",
     "rageui/MenuController.lua",
 
