f99d2a75e2a580c3c37773df66f34c0391f76552
Dylan Malandain <lucenzo.76@gmail.com>
2019-12-20 18:31:36 +0100

    __resource.lua optimized using auto load file

--- __resource.lua
+++ __resource.lua
@@ -1,44 +1,18 @@
 resource_manifest_version '05cfa83c-a124-4cfa-a768-c24a5811d8f9'
 
 client_scripts {
-    "rage-ui/menu/RageUI.lua",
-    "rage-ui/menu/Menu.lua",
-    "rage-ui/menu/MenuController.lua",
+    "rage-ui/menu/*.lua",
 
-    "components/Rectangle.lua",
-    "components/Sprite.lua",
-    "components/Text.lua",
-    "components/Visual.lua", --- Need rework
-    "components/Audio.lua",
-    "components/Scaleform.lua",
-    "components/Screen.lua",
+    "components/*.lua",
 
-    "rage-ui/menu/elements/ItemsBadge.lua",
-    "rage-ui/menu/elements/ItemsColour.lua",
-    "rage-ui/menu/elements/PanelColour.lua",
-    "rage-ui/menu/items/UIButton.lua",
-    "rage-ui/menu/items/UICheckBox.lua",
-    "rage-ui/menu/items/UIList.lua",
-    "rage-ui/menu/items/UIProgress.lua",
-    "rage-ui/menu/items/UISlider.lua",
-    "rage-ui/menu/items/UISliderProgress.lua",
-    "rage-ui/menu/items/UISliderHeritage.lua",
+    "rage-ui/menu/elements/*.lua",
 
-    "rage-ui/menu/panels/UIColourPanel.lua",
-    "rage-ui/menu/panels/UIGridPanel.lua",
-    "rage-ui/menu/panels/UIGridPanelHorizontal.lua",
-    "rage-ui/menu/panels/UIGridPanelVertical.lua",
-    "rage-ui/menu/panels/UIPercentagePanel.lua",
-    "rage-ui/menu/panels/UIStatisticsPanel.lua",
+    "rage-ui/menu/items/*.lua",
+
+    "rage-ui/menu/panels/*.lua",
     
-    "rage-ui/menu/panels/UIWeaponPanel.lua",
-    "rage-ui/menu/windows/UIHeritage.lua",
-
-    "rage-ui/timerbar/TimerBar.lua",
-    "rage-ui/timerbar/items/UIBasic.lua",
-    "rage-ui/timerbar/items/UIBarIcon.lua",
-
-
+    "rage-ui/menu/panels/*.lua",
+    "rage-ui/menu/windows/*.lua",
 
     "example.lua",
 }
